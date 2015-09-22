e2fae0661967cba9b937d93e7e290c421b2090f0
pedrosrodrigues <pedrorodrigues.idl@gmail.com>
2015-09-22 18:29:00 +0100

    Vou para casa! Amanhã há mais..

--- JobPositions/JobPositionsWebFinal/src/main/java/backingbeans/UserBean.java
+++ JobPositions/JobPositionsWebFinal/src/main/java/backingbeans/UserBean.java
@@ -5,8 +5,13 @@ import interfaces.IUser;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import entities.UserEntity;
 
@@ -24,9 +29,24 @@ public class UserBean implements Serializable {
 	private String password;
 	private String role;
 
+	private static final Logger log = LoggerFactory.getLogger(UserBean.class);
+
 	public void updateUser() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		log.info("Updating user...");
 		UserEntity uent = new UserEntity();
-		iu.updateUser(uent);
+		uent.setEmail(this.email);
+		uent.setName(this.name);
+		uent.setPassword(this.password);
+		try {
+			iu.updateUser(uent);
+			log.info("User updated!");
+			context.addMessage(null, new FacesMessage("User updated!"));
+		} catch (Exception e) {
+			log.info("Problem updating user!");
+			context.addMessage(null, new FacesMessage("Problem upating user!"));
+			e.printStackTrace();
+		}
 	}
 
 	public String getName() {
