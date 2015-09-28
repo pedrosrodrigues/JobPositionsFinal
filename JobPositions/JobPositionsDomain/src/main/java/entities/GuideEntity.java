e04273136ccfe50659bbed871f19eb70a40b5706
FilipaPedrosa <filipapedrosa@gmail.com>
2015-09-28 12:34:03 +0100

    ai

--- JobPositions/JobPositionsDomain/src/main/java/entities/GuideEntity.java
+++ JobPositions/JobPositionsDomain/src/main/java/entities/GuideEntity.java
@@ -3,12 +3,14 @@ package entities;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class GuideEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
