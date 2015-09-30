a6808388a9714ce5c385a03bafabdc5c3c5d4698
FilipaPedrosa <filipapedrosa@gmail.com>
2015-09-30 12:11:47 +0100

    changes, changes,...

--- JobPositions/JobPositionsServices/src/main/java/services/InterviewImp.java
+++ JobPositions/JobPositionsServices/src/main/java/services/InterviewImp.java
@@ -1,11 +1,13 @@
 package services;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import dao.InterviewDAO;
 import entities.InterviewEntity;
 import interfaces.IInterview;
 
+@Stateless
 public class InterviewImp implements IInterview {
 	
 	@Inject
